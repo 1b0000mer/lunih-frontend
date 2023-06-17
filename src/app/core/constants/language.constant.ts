9f22ef675b78e8aef6495c9fc35d2ce4c8976f95
Nguyễn Thiên Quốc <thienquoc98@gmail.com>
2023-06-17 15:35:14 +0300

    Update language.constant.ts
    Linda's translation lv
    
    Co-Authored-By: lindazel <106240063+lindazel@users.noreply.github.com>

--- src/app/core/constants/language.constant.ts
+++ src/app/core/constants/language.constant.ts
@@ -191,27 +191,189 @@ export const LanguageConstant = {
   },
 
   lv: {
-    HOMEPAGE: 'Homepage lv',
-    ADMIN_SYSTEM: 'Administration system lv',
+    HOMEPAGE: 'Sākums',
+    ADMIN_SYSTEM: 'Administrācija',
 
     //Button action
-    ADD: 'Add lv',
-    EDIT: 'Edit lv',
-    DELETE: 'Delete lv',
+    ADD: 'Pievienot',
+    EDIT: 'Labot',
+    DELETE: 'Dzēst',
+    CONFIRM: 'Apstiprināt',
+    CANCEL: 'Atcelt',
+    SAVE: 'Saglabāt',
+    //Modal
+    EDIT_TITLE: 'Labot formu ',
+    ADD_TITLE: 'Izveidot formu',
 
-    //Error System
+    //System
     ERR_SYSTEM: 'Something went wrong. lv',
-    ERR_CONNECT_SERVER: 'Unable to connect to server! lv',
-    ACCOUNT_WITHOUT_PRIVILEDGE: 'You do not have access right lv',
+    ERR_CONNECT_SERVER: 'Nav iespējams sazināties ar serveri!',
+    ACCOUNT_WITHOUT_PRIVILEDGE: 'Jums nav piekļuves tiesības!',
+
+    MSG_UPDATE_DONE: 'Informācija atjaunota!',
+    MSG_CREATE_DONE: 'Veiksmīgi izveidots!',
+    MSG_CHANGE_DONE: 'Informācija atjaunota!',
+    CONFIRM_DELETE: 'Vai tiešām vēlies dzēst šo ierakstu?',
+    CONFIRM_CHANGE_STATUS: 'Vai tiešām vēlies mainīt statusu šim ierakstam?',
+    CAN_NOT_BE_EMPTY:'Šī sadaļa nevar būt tukša!',
+
+    //Study level
+    BACHELOR: 'Bachelor lv',
+    MASTER: 'Master lv',
+    DOCTORAL: 'Doctoral lv',
 
     //Authentication
-    USER_INFO: 'User Information lv',
-    LOG_IN: 'Log In lv',
-    LOG_OUT: 'Izlogoties',
+    USER_INFO: 'Lietotāja informācija',
+
+    USER_NAME: 'Lietotājvārds',
+    USER_NAME_TIP: 'Lietotājvārds/E-pasts',
+    USER_NAME_ERR: 'Lietotājvārds nevar būt tukšs!',
+    
+    PASSWORD: 'Parole',
+    PASSWORD_TIP: 'Tava parole',
+    PASSWORD_ERR: 'Parole nevar būt tukša',
+
+    REMEMBER_ME: 'Atcerēties mani',
+    FORGOR: 'Aizmirsi paroli?',
+    LOG_IN: 'Pierakstīties',
+    LOG_IN_GOOGLE: 'Log In with Google',
+    SIGN_UP: 'Reģistrēties',
+    LOG_OUT: 'Izrakstīties',
+    
+    OR: 'Vai',
+    NO: 'Nē', //skaitliskā?
+    STATUS: 'Statuss',
+    ACTIVE: 'Aktīvs',
+    INACTIVE: 'Neaktīvs',
+
+    //Paginate
+    NUMBER_LIST: 'Dalībnieku skaits', //preces numurs?
+    PAGE: 'lapa',
+    TOTAL: 'Kopā',
+    ITEM: 'dalībnieks(i)',
 
     //Faculty
-    FACULTY_NAME: 'Faculty Name LV',
-    FACULTY_NAME_EN: 'Faculty English Name LV',
-    FACULTY_NAME_LV: 'Faculty Latvian Name LV',
+    FACULTY_NAME: 'Fakultātes nosaukums',
+    FACULTY_NAME_EN: 'Fakultātes nosaukums EN',
+    FACULTY_NAME_LV: 'Fakultātes nosaukums LV',
+
+    //Program
+    PROGRAM_NAME: 'Program Name lv',
+    STUDY_LEVEL: 'Study Level lv',
+    PROGRAM_NAME_EN: 'Program English Name lv',
+    PROGRAM_NAME_LV: 'Program Latvian Name lv',
+    CHOOSE_STUDY_LEVEL: 'Choose study level... lv',
+    CHOOSE_PROGRAM: 'Choose program... lv',
+
+    //Industry
+    INDUSTRY_NAME: 'Industry Name lv',
+    INDUSTRY_NAME_EN: 'Industry English Name lv',
+    INDUSTRY_NAME_LV: 'Industry Latvian Name lv',
+    CHOOSE_INDUSTRY: 'Choose industry, mulitple allowed... lv',
+
+    //Post Type
+    POST_TYPE_NAME: 'Post Type lv',
+    POST_TYPE_NAME_EN: 'Post Type in English lv',
+    POST_TYPE_NAME_LV: 'Post Type in Latvian lv',
+
+    //Homepage
+    SEARCH: 'Meklē...',
+    CATEGORIES: 'Kategorijas', 
+    ANNOUNCEMENT: 'Paziņojums',
+    THESIS_TOPIC: 'Ideju tēmas',
+    JOB_TOPIC: 'Darba piedāvājumi',
+    
+    MORE: 'Apskatīt vēl',
+
+    CONTACT_US: 'Sazināties ar mums',
+    ABOUT_US: 'Par mums',
+
+    //Header
+    STUDENT: 'Students',
+    UNIVERSITY: 'Universitāte',
+    COMPANY: 'Kompānija',
+    ADMIN: 'Administrators',
+
+    //Footer
+    INFO_1: 'Liepājas Universitātes ideju banka',
+    INFO_2: 'Ideju tēmas | Darba piedāvājums',
+    INFO_3: 'Izstrādes stadijā',
+
+    ADDRESS_1: 'Address line 1',
+    ADDRESS_2: 'Address line 2',
+    ADDRESS_3: 'Address line 3',
+
+    PHONE_NUM: '(+371) 2345 6789',
+    EMAIL: 'contact@lunih.lv',
+    DESIGN_COST: 'Developed by lunih - Liepaja University',
+
+    //Categories
+    DASHBOARD: 'Dashboard',
+    FACULTY: 'Fakultāte',
+    PROGRAM: 'Programma',
+    INDUSTRY: 'Industrija',
+    POST_TYPE: 'Publikācijas veds',
+    USER_ACCOUNT: 'Lietotājs & Konts',
+    POST_MANAGEMENT: 'Publikācijas menedžments',
+    POST: 'Publikācija',
+    APPROVE_STUDENT: 'Approve Student lv',
+    ADDRESS: 'Address lv',
+    PHONE_NUMBER: 'Phone number lv',
+
+    //User_account
+    ACCOUNT_EMAIL: 'Email lv',
+    ACCOUNT_PASSWORD: 'Password lv',
+    GENDER: 'Gender lv',
+    CHOOSE_GENDER: '(Optional) Select gender... lv',
+    MALE: 'Male lv',
+    FEMALE: 'Female lv',
+    OTHER: 'Other lv',
+    MR: 'Mr.',
+    MRS: 'Mrs.',
+    MS: 'Ms.',
+
+    //Student
+    STUDENT_ID: 'Student ID',
+    STUDENT_FIRST_NAME: 'Name lv',
+    STUDENT_SUR_NAME: 'Surname lv',
+    BIRTHDAY: 'Birthday lv',
+    CHOOSE_BIRTHDAY: '(Optional) Select date...',
+
+    //University
+    UNIVERSITY_NAME: 'Fullname lv',
+
+    //Company
+    COMPANY_NAME: 'Company Name lv',
+    COMPANY_TYPE: 'Company Type lv',
+    COMPANY_CONTACT_PERSON_NAME: 'Person name lv',
+    COMPANY_DESCRIPTION: 'Company Description lv',
+    COMPANY_PRIVATE: 'Private',
+    COMPANY_PUBLIC: 'Public',
+    COMPANY_NGO: 'NGO',
+    COMPANY_GOVERNMENT: 'Government',
+    COMPANY_EDUCATIONAL: 'Educational',
+    COMPANY_OTHER: 'Other',
+    COMPANY_ADDRESS: 'Company Address lv',
+    COMPANY_WEBSITE: 'Company Website lv',
+    COMPANY_CONTACT_PERSON_TITLE: 'Person Title lv',
+    COMPANY_CONTACT_PERSON_EMAIL: 'Person Email lv',
+    COMPANY_CONTACT_PERSON_DEPARTMENT: 'Person Department lv',
+    COMPANY_CONTACT_PERSON_PHONE: 'Person phone number lv',
+
+    //Post
+    POST_TITLE_EN: 'Post title in English lv',
+    POST_TITLE_LV: 'Post title in Latvian lv',
+    POST_DESCRIPTION_EN: 'Post description in English lv',
+    POST_DESCRIPTION_LV: 'Post description in Latvian lv',
+    POST_NUM_SLOT: 'Number of slots lv',
+    POST_START_END_DATE: 'Post start & end date lv',
+    POST_START_DATE: 'Start date lv',
+    POST_END_DATE: 'End date lv',
+    POST_STUDENT_LIST: 'Student applied list lv',
+    POST_QUEUE_LIST: 'Student queue list lv',
+
+    POST_TITLE: 'Post title lv',
+    NUM_SLOT: 'Slots lv',
+
   }
 };
